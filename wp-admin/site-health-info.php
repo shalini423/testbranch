3be6a85f06b8bf29ef0f586d1b96c965afe301be
Shalini Rakam (TECH MAHINDRA LTD.) <v-shrak@microsoft.com>
2019-08-27 06:01:59 -0700

    added

--- wp-admin/site-health-info.php
+++ wp-admin/site-health-info.php
@@ -11,7 +11,7 @@ require_once( dirname( __FILE__ ) . '/admin.php' );
 
 $title = __( 'Site Health Info' );
 
-if ( ! current_user_can( 'view_site_health_checks' ) ) {
+if ( ! current_user_can( 'install_plugins' ) ) {
 	wp_die( __( 'Sorry, you are not allowed to access the debug data.' ), '', 403 );
 }
 
--- wp-admin/site-health.php
+++ wp-admin/site-health.php
@@ -16,7 +16,7 @@ require_once( dirname( __FILE__ ) . '/admin.php' );
 
 $title = __( 'Site Health Status' );
 
-if ( ! current_user_can( 'view_site_health_checks' ) ) {
+if ( ! current_user_can( 'install_plugins' ) ) {
 	wp_die( __( 'Sorry, you are not allowed to access site health information.' ), '', 403 );
 }
 
@@ -52,17 +52,11 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 
 	<nav class="health-check-tabs-wrapper hide-if-no-js" aria-label="<?php esc_attr_e( 'Secondary menu' ); ?>">
 		<a href="<?php echo esc_url( admin_url( 'site-health.php' ) ); ?>" class="health-check-tab active" aria-current="true">
-			<?php
-			/* translators: tab heading for Site Health Status page */
-			_ex( 'Status', 'Site Health' );
-			?>
+			<?php _e( 'Status' ); ?>
 		</a>
 
 		<a href="<?php echo esc_url( admin_url( 'site-health.php?tab=debug' ) ); ?>" class="health-check-tab">
-			<?php
-			/* translators: tab heading for Site Health Info page */
-			_ex( 'Info', 'Site Health' );
-			?>
+			<?php _e( 'Info' ); ?>
 		</a>
 	</nav>
 </div>
